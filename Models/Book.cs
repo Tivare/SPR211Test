4a3def8ca239583b7045b7d3112e54b00ffa4333
Khylko Andrii <hilko_a@itstep.academy>
2024-07-06 10:06:54 +0300

    Add model Book

---
+++ Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject.Models
+{
+    internal class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string? Authtor { get; set; }
+        public decimal Price { get; set; }
+        public override string ToString()
+        {
+            return $"{Name}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-﻿namespace MyProject
+﻿using MyProject.Models;
+
+namespace MyProject
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           var book = new Book() { Id = 1, Name = "Harry Potter", Authtor="J.K.Rowling", Price = 1000};
+            Console.WriteLine(book);
+
         }
     }
 }
